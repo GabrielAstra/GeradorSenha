9d7e1fa99fb73baa0b4a5a72bd7e3d18d31f3a12
GabrielAstra <gvlc2013@gmail.com>
2023-08-20 22:10:14 -0300

    Gerador

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace GeradorSenhaComArquivo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int comprimento = 12; 
+            string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()"; 
+            Random random = new Random();
+
+            char[] password = new char[comprimento];
+            for (int i = 0; i < comprimento; i++)
+            {
+                int index = random.Next(caracteres.Length);
+                password[i] = caracteres[index];
+            }
+
+            string senhaAleatoria = new string(password);
+
+            Console.Write("Informe onde a senha será usada: ");
+            string usoSenha = Console.ReadLine();
+
+            string conteudoArquivo = $"Senha para uso em: {usoSenha}\nSenha: {senhaAleatoria}";
+
+            string documentosPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            string nomeArquivo = $"senha_{DateTime.Now:yyyyMMddHHmmss}.txt";
+
+            string caminhoCompleto = Path.Combine(documentosPath, nomeArquivo);
+
+            File.WriteAllText(caminhoCompleto, conteudoArquivo);
+
+            Console.WriteLine($"Senha gerada e salva no arquivo: {caminhoCompleto}");
+        }
+    }
+}
